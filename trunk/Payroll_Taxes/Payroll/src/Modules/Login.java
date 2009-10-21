da9436296445e54875f84c472846e33d1bff1d16
mavispuford <mavispuford@9a98b4c6-ba6b-11de-beb5-195313721cbb>
2009-10-21 01:34:00 +0000

    Testing out the taxes calculations.  Let me know if this works.

---
+++ trunk/Payroll_Taxes/Payroll/src/Modules/Login.java
@@ -0,0 +1,59 @@
+
+package Modules;
+import payroll.*;
+import java.util.Scanner;
+
+/**
+ * Used for employee authentication
+ * @author bross
+ */
+public class Login {
+
+    /**
+     *  This module handles the user ID/password authentication.
+     *  @return  false to stop looping
+     */
+    public Boolean Update()
+    {
+        int loginID = 0;
+        String loginPassword = "";
+        
+        //Make two scanners because if we use one, it causes a null pointer
+        //exception because we're taking in an int then a string
+        Scanner inputID = new Scanner(System.in);
+        Scanner inputPass = new Scanner(System.in);
+
+        //Prompt for ID number
+        System.out.println("Please enter your employee ID number: ");
+        //Make sure it's an Integer, otherwise it throws an exception
+        if (inputID.hasNextInt())
+            loginID = inputID.nextInt();
+
+        //Prompt for Password
+        System.out.println("Please enter your password: ");
+        loginPassword = inputPass.nextLine();
+
+        //Check each employee in the list for the login ID and password
+        for (Employee emp : Globals.Employees)
+        {
+            if (emp.getEmployeeID() == loginID)
+                if (emp.getPassword().equals(loginPassword))
+                {
+                    //If successful, welcome the user and change the program state
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n");
+                    System.out.print("Welcome, " + emp.getFirstName() + "!");
+                    Globals.currentUser = emp;
+                    //Stop looping
+                    return false;
+                }
+        }
+
+        //WRONG!!!
+        System.out.println("Incorrect credentials.  " +
+                           "Please make sure you typed them in correctly.");
+
+        //Keep looping
+        return true;
+    }
+
+}
---
+++ trunk/Payroll_Taxes/Payroll/src/payroll/MainProgram.java
@@ -0,0 +1,166 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package payroll;
+import Modules.*;
+import java.util.Scanner;
+
+/**
+ * The main program class.  This handles all the modules and determines what
+ * to do depending on the value of Globals.currentState.
+ * @author bross
+ */
+public class MainProgram {
+    //Declare the modules
+    MainMenu mainMenu;
+    Test testing;
+    EmployeeList employeeList;
+    Login loginScreen;
+    TimeClock timeClock;
+    AdminMenu adminMenu;
+
+    /**
+     * Default constructor; Initializes the modules etc.
+     */
+    public MainProgram()
+    {
+        mainMenu = new MainMenu();
+        testing = new Test();
+        employeeList = new EmployeeList();
+        loginScreen = new Login();
+        timeClock = new TimeClock();
+        adminMenu = new AdminMenu();
+
+        //Display the welcome message
+        DisplayWelcome();
+        //Load the employees from the database
+        Serialize.RetrieveEmployees();
+    }
+
+    /**
+     * Checks Globals.currentState to determine what to do next.
+     */
+    public void Update()
+    {
+        switch (Globals.currentState)
+        {
+            //Display the login screen
+            case Login:  LoginScreen(); break;
+            //Display the login screen
+            case TimeClock:  TimeClock(); break;
+            //Display the menu
+            case MainMenu: MainMenu(); break;
+            //Display the menu
+            case AdminMenu: AdminMenu(); break;
+            //List of Employees
+            case EmployeeList: EmployeeList(); break;
+            //Runs the test module
+            case Test: Testing(); break;
+            //Exit the program...
+            case Quit: exitProgram(); break;
+            //Banana
+        }
+    }
+
+    /**
+     * Displays the program welcome screen.
+     */
+    public static void DisplayWelcome()
+    {
+        System.out.println("/|*****************************************************|");
+        System.out.println("/|*****************************************************|");
+        System.out.println("/|** PAYROLL TO THE MAX! (MAX) (max) (max) <--Echoes **|");
+        System.out.println("/|*****************************************************|");
+        System.out.println("/|*****************************************************|");
+        System.out.println("/////////////////////////////////////////////////////// ");
+    }
+
+    /**
+     * Loops the login module
+     */
+    public void LoginScreen()
+    {
+        while(loginScreen.Update())
+        {}
+
+        Globals.currentState = Globals.State.MainMenu;
+    }
+
+    /**
+     * Loops through the employeeList update until false
+     * @see Modules.EmployeeList
+     */
+    public void EmployeeList()
+    {
+        while (employeeList.Update())
+        {}
+
+        Globals.currentState = Globals.State.MainMenu;
+    }
+
+    /**
+     * Loops through the mainMenu update until false
+     * @see Modules.MainMenu
+     */
+    public void MainMenu()
+    {
+        while (mainMenu.Update())
+        {}
+    }
+
+    /**
+     * Loops through the adminMenu update until false
+     * @see Modules.AdminMenu
+     */
+    public void AdminMenu()
+    {
+        while (adminMenu.Update())
+        {}
+
+    }
+
+    /**
+     * Loops through the timeClock update until false
+     * @see Modules.TimeClock
+     */
+    public void TimeClock()
+    {
+        timeClock.LoadTimeSheet();
+        while (timeClock.Update())
+        {}
+
+        Globals.currentState = Globals.State.MainMenu;
+    }
+
+    /**
+     * Loops through the testing module until false, then goes back to the menu.
+     * @see Modules.Test
+     */
+    public void Testing()
+    {
+        while (testing.Update())
+        {}
+
+        Globals.currentState = Globals.State.MainMenu;
+    }
+
+    /**
+     * Asks the user to confirm they want to exit, then exits.
+     */
+    public void exitProgram()
+    {
+        Scanner in = new Scanner(System.in);
+        int confirm = 0;
+
+        System.out.print("Please reenter 9 to quit: ");
+        confirm = in.nextInt();
+        if (confirm == 9)
+            Globals.ExitProgram = true;
+        else
+            System.out.print("Returning to menu...");
+    }
+
+
+}
